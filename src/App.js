87ae9a37848aae3c44da763abf60df636a4e9e1b
Brem0827 <guswhd284@naver.com>
2023-03-22 16:26:40 +0900

    UpDate React
    2023/03/22

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ function App() {
         <ul>
           <li><Link to="./login">로그인</Link></li>
           <li><Link to="./signup">회원가입</Link></li>
+          <li><Link to="./mypage">마이페이지</Link></li>
+          <li><Link to="./team">팀</Link></li>
         </ul>
       </header>
         <Routes>
             <Route exact path="./login" component={Login}></Route>
             <Route exact path="./signup" component={SignUp}></Route>
+            <Route exact path="./mypage" component={MyPage}></Route>
+            <Route exact path="./team" component={Team}></Route>
         </Routes>
     </div>
   );
---
+++ src/js/team.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import '../css/team.css';
+
+function Team(){
+    const [teamNames, setTeamNames] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  const addTeam = (e) => {
+    e.preventDefault();
+    const teamName = e.target.elements.teamName.value.trim();
+    if (teamName) {
+      setTeamNames([...teamNames, teamName]);
+      setShowForm(false);
+      e.target.elements.teamName.value = "";
+    }
+  };
+
+  const removeTeam = (indexToRemove) => {
+    setTeamNames(teamNames.filter((_, index) => index !== indexToRemove));
+  };
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleStarClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <div className="App">
+      {showForm ? (
+        <form onSubmit={addTeam}>
+          <input type="text" name="teamName" placeholder="팀 이름을 입력 해 주세요" />
+          <button className="TeamButton" type="submit">추가</button>
+          <button className="TeamButton" type="button" onClick={() => setShowForm(false)}>
+            취소
+          </button>
+        </form>
+      ) : (
+        <div>
+            <button className="plus" onClick={() => setShowForm(true)}>+</button>  팀 생성 / 추가
+        </div>
+      )}
+
+      {teamNames.map((teamName, index) => (
+        <div key={teamName} className="team">
+          <div className="icon">{teamName.charAt(0)}</div>
+          <div className="name">{teamName}</div>
+          <button className="RemoveButton" onClick={() => removeTeam(index)}>삭제</button>
+        </div>
+      ))}
+    </div>
+  );
+  }
+  
+
+export default Team;
