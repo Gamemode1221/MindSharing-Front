0780e3803560b28ddaa13259ee9b8948d551b8b2
Brem <62270266+Brem0827@users.noreply.github.com>
2023-04-30 21:17:10 +0900

    Update Axios
    2023/04/30

--- src/js/login.jsx
+++ src/js/login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 import '../css/login.css';
 import SignUp from '../js/signup';
 import Menu from '../js/menu';
@@ -7,7 +8,7 @@ import Menu from '../js/menu';
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [error] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -17,28 +18,51 @@ function LoginPage() {
     setPassword(event.target.value);
   };
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-  
-    fetch('/login', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => {
-        if (response.status === 200 && response.ok) {
-          // 로그인 성공
-          window.location.href = '/';
-        } else {
-          // 로그인 실패
-          alert('잘못된 사용자 이름 또는 비밀번호입니다.');
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        alert('로그인 중 오류가 발생했습니다.');
+
+    try {
+      const response = await axios.post('/login', {
+        username,
+        password
       });
+
+      if (response.status === 200) {
+        // 로그인 성공
+        window.location.href = '/';
+      } else {
+        // 로그인 실패
+        setError('잘못된 사용자 이름 또는 비밀번호입니다.');
+      }
+    } catch (error) {
+      console.error(error);
+      setError('로그인 중 오류가 발생했습니다.');
+    }
   };
+
+  //const handleLogin = (event) => {
+  //  event.preventDefault();
+  
+  //  fetch('/login', {
+  //     method: 'POST',
+  //    body: JSON.stringify({ username, password }),
+  //    headers: { 'Content-Type': 'application/json' },
+  //  })
+  //    .then((response) => {
+  //      if (response.status === 200 && response.ok) {
+  //        // 로그인 성공
+  //        window.location.href = '/';
+  //      } else {
+  //        // 로그인 실패
+  //        alert('잘못된 사용자 이름 또는 비밀번호입니다.');
+  //      }
+  //    })
+  //    .catch((error) => {
+  //     console.error(error);
+  //      alert('로그인 중 오류가 발생했습니다.');
+  //    });
+  //};
+  
 
   return (
     <div className="login-container">
@@ -47,10 +71,22 @@ function LoginPage() {
         <div className="form-group">
             <h2 className="login">로그인</h2>
             <label>
-                <input type="text" id="useremail" name="useremail" value={username} onChange={handleUsernameChange} />
+            <input
+              type="text"
+              id="useremail"
+              name="useremail"
+              value={username}
+              onChange={handleUsernameChange}
+            />
             </label><br/>
             <label>
-                <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
             </label>
             <label>
                 <a className="explanation">계정이 없으신가요? <span><Link to="../signup">회원가입</Link></span></a>
