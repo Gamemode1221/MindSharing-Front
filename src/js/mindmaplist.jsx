dca045aed90e4fae937b256ec74edf1a2c510b53
Brem0827 <guswhd284@naver.com>
2023-05-10 15:51:46 +0900

    Update MindMapList
    2023/05/10

--- src/js/mindmaplist.jsx
+++ src/js/mindmaplist.jsx
@@ -5,7 +5,9 @@ import background from '../img/background-gray.jpg';
 
 function Mindmaplist() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAnnouncementModalOpen, setIsAnnouncementModalOpen] = useState(false);
   const [titles, setTitles] = useState([]);
+  const [announcement, setAnnouncement] = useState([]);
 
   const userData = {
     teamname: '공유마인드',
@@ -15,23 +17,38 @@ function Mindmaplist() {
     setIsModalOpen(true);
   };
 
+  const openAnnouncementModal = () => {
+    setIsAnnouncementModalOpen(true);
+  };
+
   const closeModal = () => {
     setIsModalOpen(false);
+  };
+
+  const closeAnnouncementModal = () => {
+    setIsAnnouncementModalOpen(false);
   };
 
   const handleAddClick = () => {
     openModal();
   };
 
+  const handleAnnouncementAddClick = () => {
+    openAnnouncementModal();
+  };
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
     const newTitle = event.target.title.value;
-    if (newTitle.length > 12) {
-      alert("최대 12글자까지 입력 가능합니다.");
-      return;
-    }
     setTitles([...titles, newTitle]);
     closeModal();
+  };
+
+  const handleAnnouncementAddSubmit = (event) => {
+    event.preventDefault();
+    const newAnnouncement = event.target.title.value;
+    setAnnouncement([...announcement, newAnnouncement]);
+    closeAnnouncementModal();
   };
 
   return (
@@ -44,9 +61,32 @@ function Mindmaplist() {
         <Menu />
         <div className="team-info">
           <button className="team-name-button">{userData.teamname}</button>
-          <button className="notice-button">공지사항</button>
+          <button className="notice-button" onClick={handleAnnouncementAddClick}>공지사항</button>
         </div>
       </div>
+
+      {isAnnouncementModalOpen && (
+        <div className="Announcement-List-modal-overlay">
+          <div className="Announcement-List-modal">
+            <form onSubmit={handleAnnouncementAddSubmit}>
+              <div className="Announcement-List-modal-header">
+                <h3>공지사항</h3>
+              </div>
+              <div className="Announcement-List-modal-body">
+                <input type="text" className="Announcement-title" name="title" required />
+              </div>
+              <div className="Announcement-List-modal-footer">
+                <button className="Announcement-List-Submit-button" type="submit">
+                  완료
+                </button>
+                <button className="Announcement-List-close-button" onClick={closeAnnouncementModal}>
+                  닫기
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
 
       {isModalOpen && (
         <div className="MindMap-List-modal-overlay">
@@ -77,6 +117,15 @@ function Mindmaplist() {
             style={{ width: '140px', height: '140px', borderRadius: 20 }}
           />
           <div className="mindlist-title">{title}</div>
+        </div>
+      ))}
+      {announcement.map((title) => (
+        <div className="Announcement-item" key={title}>
+          <img
+            src={background}
+            style={{ width: '140px', height: '140px', borderRadius: 20 }}
+          />
+          <div className="Announcement-title">{title}</div>
         </div>
       ))}
     </div>
